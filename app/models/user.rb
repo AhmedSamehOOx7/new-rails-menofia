fe3f6100d6745f053440b0f97a608aa4bdedbb00
AhmedSamehOOx7 <ahmedsameh300025@gmail.com>
2026-04-17 15:46:38 +0200

    add form for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :name, :dob, :email, :phone, presence: true
+
+  # Email format validation
+  validates :email,
+            format: { with: URI::MailTo::EMAIL_REGEXP },
+            uniqueness: true
 end
